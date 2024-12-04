fefff8afa2cc642e88d8d31499e23429ebc41f13
Tabakaev Mihail <mihakgma86@hotmail.com>
2024-12-04 22:52:45 +0700

    fixed files with auto edit hotkey...

--- main.py
+++ main.py
@@ -16,4 +16,5 @@ async def say_hello(name: str):
     if name in navigate_info:
         return FileResponse(navigate_info[name])
     else:
-        return {"message": f"Hello {name}"}
+        out = {"message": f"Hello {name}"}
+        return out
